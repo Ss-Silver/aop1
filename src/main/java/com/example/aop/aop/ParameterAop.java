c0ca3199535655fdf43f83b2c3aca9f182dff1c8
sangeun.seong <sangeun.seong@timf.co.kr>
2022-01-10 19:11:43 +0900

    aop_1 finish

--- src/main/java/com/example/aop/aop/ParameterAop.java
+++ src/main/java/com/example/aop/aop/ParameterAop.java
@@ -1,8 +1,11 @@
 package com.example.aop.aop;
 
-import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.annotation.Pointcut;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.*;
+import org.aspectj.lang.reflect.MethodSignature;
 import org.springframework.stereotype.Component;
+
+import java.lang.reflect.Method;
 
 @Aspect
 @Component
@@ -12,6 +15,25 @@ public class ParameterAop {
     // controller아래 있는 모든 파일에 적용
     private void cut() {
 
+    }
+    @Before("cut()")
+    public void before(JoinPoint joinPoint) {
+        //Method이름 출력
+        MethodSignature methodSignature = (MethodSignature)joinPoint.getSignature();
+        Method method = methodSignature.getMethod();
+        System.out.println(method.getName());
+
+        Object[] args = joinPoint.getArgs();
+        for(Object obj : args) {
+            System.out.println("type : " + obj.getClass().getSimpleName());
+            System.out.println("value : " + obj);
+
+        }
+    }
+    @AfterReturning(value = "cut()", returning = "returnObj")
+    public void afterReturn(JoinPoint joinPoint, Object returnObj){
+        System.out.println("returnObj");
+        System.out.println(returnObj);
     }
 
 }
